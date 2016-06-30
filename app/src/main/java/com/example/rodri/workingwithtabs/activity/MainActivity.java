7ef63379b9e96f9cb7f4cad5b6518c7d1ec184bf
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-06-30 10:21:46 -0300

    create OneFragment.java

--- app/src/main/java/com/example/rodri/workingwithtabs/activity/MainActivity.java
+++ app/src/main/java/com/example/rodri/workingwithtabs/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.rodri.workingwithtabs;
+package com.example.rodri.workingwithtabs.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.rodri.workingwithtabs.R;
 
 public class MainActivity extends AppCompatActivity {
 
